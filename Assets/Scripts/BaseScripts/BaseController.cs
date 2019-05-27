095f9c9b94ed79c379feb893a2de04d305d45954
VladKivich <vladkivich@gmail.com>
2019-05-27 04:29:37 +0300

    Переделал все скрипты

---
+++ Assets/Scripts/BaseScripts/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.BaseScripts
+{
+    public abstract class BaseController
+    {
+        public bool _isActive { get; private set; }
+
+        /// <summary>
+        /// Включаем контроллер
+        /// </summary>
+        public virtual void On()
+        {
+            _isActive = true;
+        }
+
+        /// <summary>
+        /// Выключаем контроллер
+        /// </summary>
+        public virtual void Off()
+        {
+            _isActive = false;
+        }
+
+        /// <summary>
+        /// Переключаем контроллер
+        /// </summary>
+        public virtual void Switch()
+        {
+            _isActive = !_isActive;
+        }
+
+        public abstract void ControllerUpdate();
+
+        ///Для переопределения в наследниках, если это необходимо
+        public virtual void ControllerLateUpdate() { }
+    }
+}
---
+++ Assets/Scripts/Controllers/PCInputController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.BaseScripts;
+using Assets.Scripts.Models;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    class PCInputController : BaseController
+    {
+        #region Клавиатура
+
+        public float ForwardBackward { get; private set; }
+
+        public float LeftRight { get; private set; }
+
+        public bool Jump { get; private set; }
+
+        public bool Run { get; private set; }
+
+        #endregion
+
+        #region Мышь
+
+        public bool Aim { get; private set; }
+
+        public float RotationY { get; private set; }
+
+        public float RotationX { get; private set; }
+
+        public float Zoom { get; private set; }
+
+        #endregion
+
+        public PCInput PCInputModel;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="PCInputModel">Модель ввода игрока</param>
+        public PCInputController(PCInput PCInputModel)
+        {
+            this.PCInputModel = PCInputModel;
+        }
+        
+        public override void ControllerUpdate()
+        {
+            ForwardBackward = Input.GetAxis("Horizontal");
+
+            LeftRight = Input.GetAxis("Vertical");
+
+            Jump = Input.GetKeyDown(PCInputModel.JumpButton);
+
+            Run = Input.GetKey(PCInputModel.RunButton);
+
+            RotationY = Input.GetAxis("Mouse X");
+
+            RotationX = -Input.GetAxis("Mouse Y");
+
+            Aim = Input.GetMouseButton((int)PCInputModel.AimMouseButton);
+
+            Zoom = Input.GetAxis("Mouse ScrollWheel");
+        }
+    }
+}

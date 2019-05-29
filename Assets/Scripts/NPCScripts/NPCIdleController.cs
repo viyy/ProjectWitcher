709518c7ae2f008beace9d77fc538f5807e1c45a
SkyPowder <beerbl4pno@gmail.com>
2019-05-29 14:58:16 +0300

    Созданы базовые скрипты управления неигровым персонажем

---
+++ Assets/Scripts/NPCScripts/NPCIdleController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCIdleController : MonoBehaviour
+{
+    public delegate void IdleWaiter();
+    public static IdleWaiter IdleEvent;
+
+    Coroutine waiter;
+    float time;
+
+    IEnumerator WaitForEnd(float time)
+    {
+        WaitForSeconds wait = new WaitForSeconds(time);
+        yield return wait;
+        IdleEvent();
+    }
+
+    public void Idle()
+    {
+        time = Random.Range(3, 10);
+        Debug.Log("Wait: " + time);
+        waiter = StartCoroutine(WaitForEnd(time));
+    }
+}
---
+++ Assets/Scripts/NPCScripts/RouteCompile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Класс-генератор маршрута из пула точек
+/// </summary>
+public class RouteCompile : MonoBehaviour
+{
+    [SerializeField]private Transform pointsPull; //Пул точек (Необходимо добавить через инспектор)
+    private Transform[] pull;
+    int lastNum;
+
+    private void Awake()
+    {
+        lastNum = -1;
+        pull = new Transform[pointsPull.childCount];
+        for(int i = 0; i < pull.Length; i++)
+        {
+            pull[i] = pointsPull.GetChild(i);
+        }
+    }
+
+    public Transform[] Compile()
+    {
+        int length = Random.Range(4, 10);
+        Debug.Log("Length: " + length);
+        Transform[] route = new Transform[length];
+        for(int i = 0; i < length;)
+        {
+            int randPoint = Random.Range(0, pull.Length);
+            if(lastNum != randPoint)
+            {
+                route[i] = pull[randPoint];
+                lastNum = randPoint;
+                i++;
+
+            }           
+        }
+        Debug.Log("Route created");
+        return route;
+    }
+}

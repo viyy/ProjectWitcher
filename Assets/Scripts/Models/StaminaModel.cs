d8bbef5b364cabea54953b1cbe3e46771f592039
Sainell <xex-x@ya.ru>
2019-06-03 00:08:26 +0300

    update StaminaController
    fix MovementController
    
    add StaminaView

---
+++ Assets/Scripts/Models/StaminaModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    public class StaminaModel:MonoBehaviour
+    {
+        public float Stamina = 100;
+        public float StaminaMaximum = 100;
+
+        public float StaminaRunCoast = 10f;
+
+        public float StaminaJumpCoast = 20f;
+
+        public float StaminaRollCoast = 30f;
+
+        public float StaminaStandRegenRate = 25f;
+
+        public float StaminaWalkRegenRate = 5f;
+    }
+}
---
+++ Assets/Scripts/View/StaminaView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts.BaseScripts;
+using Assets.Scripts.Models;
+namespace Assets.Scripts.Views
+{
+    public class StaminaView : MonoBehaviour
+    {
+        [SerializeField] private Image _scaleImage;
+        [SerializeField] private float _currentStaminaPercent;
+        [SerializeField] private float _currentStamina;
+        [SerializeField] private StaminaModel staminaModel;
+        
+
+        void Awake()
+        {
+            staminaModel = GameObject.Find("Player").GetComponent<StaminaModel>();
+            _scaleImage = GetComponent<Image>();
+        }
+
+
+        void Update()
+        {
+            _currentStamina = staminaModel.Stamina;
+            StaminaShow();
+        }
+        private void StaminaShow()
+        {
+            _currentStaminaPercent = _currentStamina * 0.01f;
+            _scaleImage.fillAmount = _currentStaminaPercent;
+        }
+    }
+}
